3a014b1b31a44bcaabf66b380bfbf03ff8a3bc99
齐子佳 <1206521741@qq.com>
2018-12-06 14:23:58 +0800

    The first demo
    ddddddd

---
+++ test/test/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  test
+//
+//  Created by 齐子佳 on 2018/12/6.
+//  Copyright © 2018 齐子佳. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
